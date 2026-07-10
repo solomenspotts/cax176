4e3f126bed4fcef676bc0b2dedeaf4d87bc55cdb
solomenspotts <solomenspottts@gmail.com>
2026-07-10 18:44:21 -0400

    Part 1 and 2 added

---
+++ alab351_1.py
@@ -0,0 +1,17 @@
+# Part 1: 1) Understanding Programming
+# Computer programming creates instructions for computers to execute tasks that allows users to 
+# to use computer by allowing the computer hardware to work with software. Without computer 
+# #programming we would not be able to use computers.
+
+#2) Python's Appeal. 
+# Python is a high level programming language that is popular programming language for 
+# beginners because it is easy to learn and read.
+#  
+#3) In order to write and run Python you need to install Python on your computer.
+# After downloading and installing Python you can write code in a text editor, then run code in the command line or terminal. 
+# Using an intergrated development environmnent such as Visual Studio Code is also a good option for 
+# writing and running Python code.
+
+# Part 2: write simple script that prints the following output Hello, World! Welcome to Python 
+# programing
+print("Hello, World!\nWelcome to Python Programming.")
