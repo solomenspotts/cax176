3361014d67018e4b55c88c201e679b9f9629038d
solomenspotts <solomenspottts@gmail.com>
2026-07-14 14:21:47 -0400

    The word input is counted, changed to uppercase, and repeated 3 times

---
+++ string_fun.py
@@ -0,0 +1,9 @@
+# Prompts user for word
+word = input("Enter a word:") 
+# prints the length of the word
+print(f"The length of the word is {len(word)}.")
+#prints word in all uppercase
+upper_case = word.upper()
+print(upper_case)
+#Repeatly prints word 3 times 
+print((word) * 3, sep="\n")
