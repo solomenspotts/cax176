2fec027afc97be6356feb00744006204bf657567
solomenspotts <solomenspottts@gmail.com>
2026-07-24 18:16:40 -0400

    Created Task 1 String Manipulation Challenge

--- string_fun.py
+++ string_fun.py
@@ -1,9 +1,15 @@
-# Prompts user for word
-word = input("Enter a word:") 
-# prints the length of the word
-print(f"The length of the word is {len(word)}.")
-#prints word in all uppercase
-upper_case = word.upper()
-print(upper_case)
-#Repeatly prints word 3 times 
-print((word) * 3, sep="\n")
+# # Prompts user for word
+# word = input("Enter a word:") 
+# # prints the length of the word
+# print(f"The length of the word is {len(word)}.")
+# #prints word in all uppercase
+# upper_case = word.upper()
+# print(upper_case)
+# #Repeatly prints word 3 times 
+# print((word) * 3)
+
+# import math
+
+# print(dir(math), end='\n')
+
+print("Hello" + "3")
